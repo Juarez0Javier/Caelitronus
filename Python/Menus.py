04b3d9e28fda4cee8ab30749993c441dde116f4e
BriossoA <71294955+BriossoA@users.noreply.github.com>
2025-07-09 20:20:31 -0300

    añadidas clases de seleccion de nivel y personaje

--- Python/Menus.py
+++ Python/Menus.py
@@ -5,6 +5,7 @@ import pygame_widgets as pwidgets
 from pygame_widgets.button import Button
 
 import Characters
+import Button
 
 pygame.init()
 
@@ -410,6 +411,277 @@ class LvUpScreen:
         M1.lvUp()
 
         self._ExitMode = True
+
+class LevelSelectScreen:
+
+    def __init__(self,screen):
+
+        self.screen = screen
+
+        #Prueba. True si los jefes han sido derrotados (no me hago cargo si no respetan el orden de pelea)
+        self.flag_serpico = False
+        self.flag_espina = True
+        self.flag_corvus = False
+        self.flag_galaad = False
+        self.flag_misionero = False
+        self.jefesderrotados = 0
+
+        self._ExitMode = False
+
+        pass
+
+    def runMenu(self):
+
+        screen = self.screen
+
+        #Trae y muestra los iconos de los jefes
+        fondo = pygame.image.load("./Assets/BckGrnd/Nivel.png").convert()
+        fondo = pygame.transform.scale(fondo, (WIDTH, HEIGHT))
+        icon_back_color = '#000000'
+        if(self.flag_serpico == False):
+            icon_serpico = pygame.image.load("./Assets/Icons/Serpico.png")
+        else:
+            icon_serpico = pygame.image.load("./Assets/Icons/SerpicoDerrotado.png")
+            self.jefesderrotados+=1
+        icon_serpico = pygame.transform.scale(icon_serpico, (50, 50))
+        posicion = pygame.Rect(155,550,50,50)
+        icon_serpico_rect = icon_serpico.get_rect(center = posicion.center)
+        if(self.flag_espina == False):
+            icon_espina = pygame.image.load("./Assets/Icons/Espina.png")
+        else:
+            icon_espina = pygame.image.load("./Assets/Icons/EspinaDerrotado.png")
+            self.jefesderrotados+=1
+        icon_espina = pygame.transform.scale(icon_espina, (50, 50))
+        posicion = pygame.Rect(435,550,50,50)
+        icon_espina_rect = icon_espina.get_rect(center = posicion.center)
+        if(self.flag_corvus == False):
+            icon_corvus = pygame.image.load("./Assets/Icons/Corvus.png")
+        else:
+            icon_corvus = pygame.image.load("./Assets/Icons/CorvusDerrotado.png")
+            self.jefesderrotados+=1
+        icon_corvus = pygame.transform.scale(icon_corvus, (50, 50))
+        posicion = pygame.Rect(715,550,50,50)
+        icon_corvus_rect = icon_corvus.get_rect(center = posicion.center)
+        if(self.flag_galaad == False):
+            icon_galaad = pygame.image.load("./Assets/Icons/Galaad.png")
+        else:
+            icon_galaad = pygame.image.load("./Assets/Icons/GalaadDerrotado.png")
+            self.jefesderrotados+=1
+        icon_galaad = pygame.transform.scale(icon_galaad, (50, 50))
+        posicion = pygame.Rect(435,275,50,50)
+        icon_galaad_rect = icon_galaad.get_rect(center = posicion.center)
+        icon_misionero = pygame.image.load("./Assets/Icons/Misionero.png")
+        icon_misionero = pygame.transform.scale(icon_misionero, (50, 50))
+        posicion = pygame.Rect(435,50,50,50)
+        icon_misionero_rect = icon_misionero.get_rect(center = posicion.center)
+
+        #Botones de Seleccion de nivel (clase Button)
+        but_serpico = Button.Button('Obispo Serpico',200,100,(80,600))
+        but_espina = Button.Button('Padre Espina',200,100,(360,600))
+        but_corvus = Button.Button('Fray Corvus',200,100,(640,600))
+        but_galaad = Button.Button('Galaad',200,100,(360,325))
+        but_misionero = Button.Button('El Misionero',200,100,(360,100))
+        but_menu = Button.Button('Salir al Menu Principal',280,50,(595,25))
+
+        #Prueba. Sale si se presiona el boton de Salir al Menu Principal (falta integrar).
+        run = True
+        while run:
+            events = pygame.event.get()
+            for event in events:
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    run = False
+                    quit()
+                if event.type == pygame.MOUSEBUTTONUP:
+                    if(but_serpico.get_clicked() == True):
+                        self._ExitMode = True
+                    if(but_espina.get_clicked() == True):
+                        self._ExitMode = True
+                    if(but_corvus.get_clicked() == True):
+                        self._ExitMode = True
+                    if(but_galaad.get_clicked() == True):
+                        self._ExitMode = True
+                    if(but_misionero.get_clicked() == True):
+                        self._ExitMode = True
+                    if(but_menu.get_clicked() == True):
+                        self._ExitMode = True
+            
+            #Rect.Rect.Rect.Rect.Rect.Rect.Rect.Rect.Rect.Rect.Rect.Rect.Rect.Rect.Rect...
+            #Dibuja las lineas entre los niveles, y sus variantes, segun cual y cuantos jefes se han derrotado
+            screen.blit(fondo,(0,0))
+            pygame.draw.rect(screen,'#000000',pygame.Rect(450,200,20,400))
+            pygame.draw.rect(screen,'#000000',pygame.Rect(170,485,20,50))
+            pygame.draw.rect(screen,'#000000',pygame.Rect(730,485,20,50))
+            pygame.draw.rect(screen,'#000000',pygame.Rect(170,485,560,20))
+            pygame.draw.rect(screen,'#000000',pygame.Rect(140,535,80,65))
+            if(self.flag_serpico == True):
+                pygame.draw.rect(screen,(255,0,0),pygame.Rect(145,540,70,65),5)
+                pygame.draw.rect(screen,(255,0,0),pygame.Rect(175,535,10,5))
+            else:
+                 pygame.draw.rect(screen,(247,236,36),pygame.Rect(145,540,70,65),5)
+            pygame.draw.rect(screen,icon_back_color,icon_serpico_rect)
+            screen.blit(icon_serpico,icon_serpico_rect)
+            pygame.draw.rect(screen,'#000000',pygame.Rect(420,535,80,65))
+            if(self.flag_espina == True):
+                pygame.draw.rect(screen,(255,0,0),pygame.Rect(425,540,70,65),5)
+                pygame.draw.rect(screen,(255,0,0),pygame.Rect(455,535,10,5))
+            else:
+                pygame.draw.rect(screen,(247,236,36),pygame.Rect(425,540,70,65),5)
+            pygame.draw.rect(screen,icon_back_color,icon_espina_rect)
+            screen.blit(icon_espina,icon_espina_rect)
+            pygame.draw.rect(screen,'#000000',pygame.Rect(700,535,80,65))
+            if(self.flag_corvus == True):
+                pygame.draw.rect(screen,(255,0,0),pygame.Rect(705,540,70,65),5)
+                pygame.draw.rect(screen,(255,0,0),pygame.Rect(735,535,10,5))
+            else:
+                pygame.draw.rect(screen,(247,236,36),pygame.Rect(705,540,70,65),5)
+            pygame.draw.rect(screen,icon_back_color,icon_corvus_rect)
+            screen.blit(icon_corvus,icon_corvus_rect)
+            pygame.draw.rect(screen,'#000000',pygame.Rect(420,260,80,65))
+            if(self.flag_galaad == True):
+                pygame.draw.rect(screen,(255,0,0),pygame.Rect(425,265,70,65),5)
+                pygame.draw.rect(screen,(255,0,0),pygame.Rect(455,200,10,65))
+            else:
+                pygame.draw.rect(screen,(247,236,36),pygame.Rect(425,265,70,65),5)
+            pygame.draw.rect(screen,icon_back_color,icon_galaad_rect)
+            screen.blit(icon_galaad,icon_galaad_rect)
+            pygame.draw.rect(screen,'#000000',pygame.Rect(420,35,80,65))
+            pygame.draw.rect(screen,(247,236,36),pygame.Rect(425,40,70,65),5)
+            pygame.draw.rect(screen,icon_back_color,icon_misionero_rect)
+            screen.blit(icon_misionero,icon_misionero_rect)
+    
+            if self.jefesderrotados == 1:
+                pygame.draw.rect(screen,(255,0,0),pygame.Rect(175,497,10,38))
+                pygame.draw.rect(screen,(255,0,0),pygame.Rect(735,497,10,38))
+                pygame.draw.rect(screen,(255,0,0),pygame.Rect(455,497,10,38))
+                pygame.draw.rect(screen,(255,0,0),pygame.Rect(175,497,560,3))
+            elif self.jefesderrotados >= 2:
+                pygame.draw.rect(screen,(255,0,0),pygame.Rect(175,490,10,45))
+                pygame.draw.rect(screen,(255,0,0),pygame.Rect(735,490,10,45))
+                pygame.draw.rect(screen,(255,0,0),pygame.Rect(455,480,10,55))
+                pygame.draw.rect(screen,(255,0,0),pygame.Rect(175,490,560,10))
+                if (self.jefesderrotados >= 3):
+                    pygame.draw.rect(screen,(255,0,0),pygame.Rect(455,425,10,55))
+
+            #Llamadas a la funcion draw de la clase Button
+            but_serpico.draw(screen,True,self.flag_serpico)
+            but_espina.draw(screen,True,self.flag_espina)
+            but_corvus.draw(screen,True,self.flag_corvus)
+            if(self.jefesderrotados >= 3):
+                but_galaad.draw(screen,True,self.flag_galaad)
+            else:
+                but_galaad.draw(screen,False,self.flag_galaad)
+            if(self.jefesderrotados >= 4):
+                but_misionero.draw(screen,True,self.flag_misionero)
+            else:
+                but_misionero.draw(screen,False,self.flag_misionero)
+            but_menu.draw(screen,True,False)
+
+            if self._ExitMode == True:
+                run = False
+            
+            pygame.display.update()
+            clock.tick(30)
+
+class CharSelectScreen:
+
+    def __init__(self,screen):
+
+        self.screen = screen
+        self._ExitMode = False
+
+        pass
+
+    def drawThing(self,screen,rect,thing):
+
+        fondo = pygame.image.load("./Assets/BckGrnd/paperallborder.png").convert()
+        border_rect = rect
+        border_color = '#000000'
+        fondo = pygame.transform.scale(fondo, (border_rect.width - 10 , border_rect.height - 10))
+        fondo_rect = fondo.get_rect(center = border_rect.center)
+
+        if isinstance(thing, str):
+
+            if(border_rect.left == 40):
+                icon = pygame.image.load("./Assets/Icons/Espada.png")
+                but_select = Button.Button('Seleccionar',150,40,(100, 670))
+            if(border_rect.left == 325):
+                icon = pygame.image.load("./Assets/Icons/Escudo.png")
+                but_select = Button.Button('Seleccionar',150,40,(385, 670))
+            if(border_rect.left == 610):
+                icon = pygame.image.load("./Assets/Icons/Velocidad.png")
+                but_select = Button.Button('Seleccionar',150,40,(670, 670))
+            
+            icon = pygame.transform.scale(icon, (32, 32))
+            icon_rect = icon.get_rect(center = fondo_rect.center)
+            icon_rect.top = border_rect.top + 15
+
+            font = pygame.font.Font("./Assets/Fonts/Seagram_tfb.ttf", 20)
+            text = font.render(thing,True,'#000000')
+            text_rect = text.get_rect(center = fondo_rect.center)
+            
+            pygame.draw.rect(screen,border_color,border_rect,5)
+            screen.blit(fondo,fondo_rect)
+            screen.blit(icon,icon_rect)
+            screen.blit(text,text_rect)
+
+            return but_select
+
+        else:
+
+            img = thing
+            img_rect = thing.get_rect(center = fondo_rect.center)
+
+            pygame.draw.rect(screen,border_color,border_rect,5)
+            screen.blit(fondo,fondo_rect)
+            screen.blit(img,img_rect)
+
+    def runMenu(self):
+
+        screen = self.screen
+
+        fondo_screen = pygame.image.load("./Assets/BckGrnd/Nivel.png").convert()
+        fondo_screen = pygame.transform.scale(fondo_screen, (WIDTH, HEIGHT))      
+        caelius1 = pygame.image.load("./Assets/ChArt/Calius.png")
+        caelius1 = pygame.transform.scale(caelius1, (200, 386))
+        caelius2 = pygame.image.load("./Assets/ChArt/Calius2.png")
+        caelius2 = pygame.transform.scale(caelius2, (200, 386))
+        caelius3 = pygame.image.load("./Assets/ChArt/Calius3.png")
+        caelius3 = pygame.transform.scale(caelius3, (200, 386))
+
+        run = True
+        while run:
+            events = pygame.event.get()
+            for event in events:
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    run = False
+                    quit()
+                if event.type == pygame.MOUSEBUTTONUP:
+                    if(but_select1.get_clicked() == True):
+                        self._ExitMode = True
+                    if(but_select2.get_clicked() == True):
+                        self._ExitMode = True
+                    if(but_select3.get_clicked() == True):
+                        self._ExitMode = True
+
+            screen.blit(fondo_screen,(0,0))
+            self.drawThing(screen, pygame.Rect(65,25,220,425), caelius1)
+            self.drawThing(screen, pygame.Rect(350,25,220,425), caelius2)
+            self.drawThing(screen, pygame.Rect(635,25,220,425), caelius3)
+            but_select1 = self.drawThing(screen, pygame.Rect(40,475,270,250), "   Fauste de Fe (Ira): \n Incrementa sus valores\n de ATK y DAN en un \n   30% por 5 segundos  ")
+            but_select2 = self.drawThing(screen, pygame.Rect(325,475,270,250), "     Fauste de Fe (Pena):\nIncrementa su valor de DEF\n  en un 40% por 5 segundos\n       y se cura 10% de sus\n            PV maximos")
+            but_select3 = self.drawThing(screen, pygame.Rect(610,475,270,250), "      Fauste de Fe (Ego):\nIncrementa su valor de SRT\n   por un 10% y sus valores\n    de ESQ y VLC en 20%\n          por 5 segundos")
+           
+            but_select1.draw(screen,True,False)
+            but_select2.draw(screen,True,False)
+            but_select3.draw(screen,True,False)            
+
+            if self._ExitMode == True:
+                run = False
+
+            pygame.display.update()
+            clock.tick(30)
 
 '''
 import Characters
